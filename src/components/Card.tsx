27da187532463aaf0f8947894faeb87e1f5992f7
Martin Cartledge <mcartledge@opentable.com>
2026-08-10 14:12:47 -0600

    chore(styling): Clean up styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,23 +10,23 @@ export interface Props {
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
   const { title, pubDatetime, description } = frontmatter;
   return (
-    <li className="my-6">
+    <li className="my-8">
       <a
         href={href}
-        className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+        className="inline-block font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
       >
         {secHeading ? (
-          <h2 className="text-lg font-medium decoration-dashed hover:underline">
+          <h2 className="text-xl font-semibold decoration-dashed hover:underline">
             {title}
           </h2>
         ) : (
-          <h3 className="text-lg font-medium decoration-dashed hover:underline">
+          <h3 className="text-xl font-semibold decoration-dashed hover:underline">
             {title}
           </h3>
         )}
       </a>
-      <Datetime datetime={pubDatetime} />
-      <p>{description}</p>
+      <Datetime datetime={pubDatetime} className="mt-2" />
+      <p className="mt-3 leading-relaxed opacity-70">{description}</p>
     </li>
   );
 }
